be589c570e22f7a320f92a2db6d3e15c87d96242
herongxing <herx@healthmall.cn>
2017-12-27 18:12:09 +0800

    config

---
+++ rabbit-message/src/test/java/dominic/config/RabbitMessageConfig.java
@@ -0,0 +1,30 @@
+package dominic.config;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by Administrator:herongxing on 2017/12/13 10:47.
+ */
+@Configuration
+@ComponentScan("dominic.message.rabbit")
+public class RabbitMessageConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix="spring.rabbitmq")
+    public ConnectionFactory rabbitMessageConnectionFactory() {
+        return new ConnectionFactory();
+    }
+
+    @Bean
+    public Connection rabbitMessageConnection(ConnectionFactory rabbitMessageConnectionFactory) throws IOException, TimeoutException {
+        return rabbitMessageConnectionFactory.newConnection();
+    }
+}
